ea63561762bf320470b5e8e22f40ad1475af404e
Mike Glover <mikeglover06@gmail.com>
2020-07-25 22:09:30 +0100

    Added allowing to download point daily releases

--- getiso.sh
+++ getiso.sh
@@ -14,17 +14,23 @@ function download() {
     echo "ERROR: $1 does not exit"
     return
 	fi
-	echo $1
+	# Check if not latest series
+	if [[ ! ${SERIES} = "groovy" ]]; then
+	folder="${1}/${SERIES}"
+	else
+	folder = "$1"
+	fi
+	echo ${folder}
 	mkdir -p $DEST$1
 	cd $DEST$1 || exit
-	zsync http://cdimage.ubuntu.com/$1/daily-live/current/$SERIES-$TYPE-$ARCH.iso.zsync
+	zsync http://cdimage.ubuntu.com/$folder/daily-live/current/$SERIES-$TYPE-$ARCH.iso.zsync
 	rm *.zs-old &> /dev/null
 	cd ..
 }
 
 function info() {
 	# get remote zsync file
-	wget -q http://cdimage.ubuntu.com/$1/daily-live/current/$SERIES-$TYPE-$ARCH.list -O /tmp/${1}.list
+	wget -q http://cdimage.ubuntu.com/$folder/daily-live/current/$SERIES-$TYPE-$ARCH.list -O /tmp/${1}.list
 	# get modified date of local file
 	if [ -f "$DEST$1/$SERIES-$TYPE-$ARCH.iso" ]; then
     	ml=$(eval "stat -c %y $DEST$1/$SERIES-$TYPE-$ARCH.iso")
@@ -43,6 +49,7 @@ function info() {
 
 if [ $1 == "--info" ];
 	then
+	printf "%25s\n" "Set to: ${SERIES}"
 	printf "%14s %11s %11s\n" "Flavour" "Local" "Remote"
 	for d in "${FLAVOURS[@]}"
 	do
